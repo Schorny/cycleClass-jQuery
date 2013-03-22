51fb919efa9ce30498202196a870944576ccf3ae
Toni Schornboeck <toni.schornboeck@gmail.com>
2013-03-22 18:10:30 +0100

    added a few new tests, including tests for new features

--- tests/tests.js
+++ tests/tests.js
@@ -129,3 +129,67 @@ test("correct roundtrip parameter", function() {
     $elem.cycleClass(classList, config);
     $elem.cycleClass(classList, config);
 });
+
+test("don't remove other classes", function() {
+    var $elem=$("<div class='other one'></div>");
+    var classList=["one", "two", "three"];
+
+    ok(testClasses($elem, ["one", "other"], ["two", "three"]), "setup");
+    $elem.cycleClass(classList);
+    ok(testClasses($elem, ["two", "other"], ["one", "three"]), "first step");
+});
+
+test("don't remove partial class names", function() {
+    var $elem=$("<div class='one oneone'></div>");
+    var classList=["one", "two", "three"];
+
+    ok(testClasses($elem, ["one", "oneone"], ["two", "three"]), "setup");
+    $elem.cycleClass(classList);
+    ok(testClasses($elem, ["two", "oneone"], ["one", "three"]), "first step");
+});
+
+test("transition call", function() {
+    var $elem=$("<div class='one'></div>");
+    var classList=["one", "two", "three"];
+
+    var transitionCalled=false;
+
+    var config={
+        transition:function() { ok(transitionCalled===false, "transition call"); transitionCalled=true; }
+    };
+
+    ok(testClasses($elem, ["one"], ["two", "three"]), "setup");
+    $elem.cycleClass(classList, config);
+    ok(testClasses($elem, ["one"], ["two", "three"]), "first step");
+
+    ok(transitionCalled===true, "transition called");
+});
+
+test("correct transition parameter", function() {
+    var $elem=$("<div class='one' data-dings='dangs'></div>");
+    var classList=["one", "two", "three"];
+
+    var config={
+        transition:function(e, oldClass, newClass) {
+            ok($(e).data("dings")==="dangs", "element parameter");
+            ok(oldClass==="one", "old class parameter");
+            ok(newClass==="two", "new class parameter");
+        }
+    };
+
+    ok(testClasses($elem, ["one"], ["two", "three"]), "setup");
+    $elem.cycleClass(classList, config);
+    ok(testClasses($elem, ["one"], ["two", "three"]), "first step");
+});
+
+test("jump to index", function() {
+    var $elem=$("<div class='one'></div>");
+    var classList=["one", "two", "three"];
+    var config={
+        toIndex: 2
+    };
+
+    ok(testClasses($elem, ["one"], ["two", "three"]), "setup");
+    $elem.cycleClass(classList, config);
+    ok(testClasses($elem, ["three"], ["one", "two"]), "first step");
+});
